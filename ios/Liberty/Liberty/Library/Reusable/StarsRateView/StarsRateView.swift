f4d7311183c03fbad3439c4f7cf6b3ea11715dae
Vlad Krupenko <dev.fixique@gmail.com>
2018-10-27 23:11:37 +0300

    prepare configure

--- ios/Liberty/Liberty/Library/Reusable/StarsRateView/StarsRateView.swift
+++ ios/Liberty/Liberty/Library/Reusable/StarsRateView/StarsRateView.swift
@@ -7,11 +7,47 @@
 //
 
 import UIKit
+import Cosmos
 
 final class StarsRateView: DesignableView {
 
     // MARK: - IBOutlets
 
-    
+    @IBOutlet private weak var backButton: UIButton!
+    @IBOutlet private weak var rateView: CosmosView!
+
+    // MARK: - UIView
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupInitialState()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupInitialState()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
+
+// MARK: - Configure
+
+private extension StarsRateView {
+
+    func setupInitialState() {
+        configureRateView()
+    }
+
+    func configureRateView() {
+        rateView.rating = 0.0
+        rateView.didFinishTouchingCosmos = { [weak self] rating in
+            guard let `self` = self else { return }
+            print(self.view.description)
+        }
+    }
 
 }
