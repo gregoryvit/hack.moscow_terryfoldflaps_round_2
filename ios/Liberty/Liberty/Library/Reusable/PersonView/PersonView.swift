507859affa08f3e2e913ecd878cc6f32d6e7256e
Vlad Krupenko <dev.fixique@gmail.com>
2018-10-27 22:08:05 +0300

    replace some method and create and layout view

---
+++ ios/Liberty/Liberty/Library/Reusable/PersonView/PersonView.swift
@@ -0,0 +1,33 @@
+//
+//  PersonView.swift
+//  Liberty
+//
+//  Created by Vlad Krupenko on 27/10/2018.
+//  Copyright © 2018 Olferuk. All rights reserved.
+//
+
+import UIKit
+
+final class PersonView: DesignableView {
+
+    // MARK: - IBOutlets
+
+
+    // MARK: - UIView
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupInitialState()
+    }
+
+}
+
+// MARK: - Configure
+
+private extension PersonView {
+
+    func setupInitialState() {
+
+    }
+
+}
