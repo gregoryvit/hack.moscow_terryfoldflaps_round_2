0073f29f7d6d4432e28f40478dfad74dd3f7dc86
Vlad Krupenko <dev.fixique@gmail.com>
2018-10-28 03:41:22 +0300

    configure xib layout

---
+++ ios/Liberty/Liberty/Screens/DetailsViewController/Adapter/Cell/BookCell/BookCell.swift
@@ -0,0 +1,33 @@
+//
+//  BookCell.swift
+//  Liberty
+//
+//  Created by Vlad Krupenko on 28/10/2018.
+//  Copyright © 2018 Olferuk. All rights reserved.
+//
+
+import UIKit
+
+final class BookCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+
+
+    // MARK: - UITableViewCell
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupInitialState()
+    }
+    
+}
+
+// MARK: - Configure
+
+private extension BookCell {
+
+    func setupInitialState() {
+
+    }
+
+}
